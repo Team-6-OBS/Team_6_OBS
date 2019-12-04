dcfc85b08f307747430347832e21a373653fc186
Michael Roberts <michael.roberts.jr217@gmail.com>
2019-12-04 06:05:38 -0500

    Implemented functionalized buy/sell routes and helper functions from microservice in main backend

--- main.py
+++ main.py
@@ -17,6 +17,326 @@ app.config['DB_NAME'] = "main_server"
 engine = db.create_engine('mysql+pymysql://' + app.config['DB_USER'] + ':' + app.config['DB_PASS'] + '@' + app.config['DB_HOST'] + '/' + app.config['DB_NAME'], pool_pre_ping=True)
 app.config['DB_CONN'] = engine.connect()
 
+######## BEGIN BUY/SELL HELPER FUNCTIONS ########
+
+def update_totals(amt, price, acc, t_type, sym):
+    
+    stock_price_total = amt * price
+
+    stock_col = sym.lower() + '_stock'
+
+    sql = 'SELECT dollars, ' + stock_col + ' FROM account_totals WHERE account = \'' + acc + '\';'
+    values = query_db(sql)
+    dollars = values[0][0]
+    stock = values[0][1]
+
+    if t_type == 'BUY':
+        updated_dollars = dollars - stock_price_total
+        updated_stock = stock + amt
+    else:
+        updated_dollars = dollars + stock_price_total
+        updated_stock = stock - amt
+
+    sql = 'UPDATE account_totals SET dollars = \'' + str(updated_dollars) + '\', '
+    sql += stock_col + '= \'' + str(updated_stock) + '\' WHERE account = \'' + acc + '\';'
+    res = query_db(sql)
+
+    return res
+
+def authenticate(auth):
+    """This function takes a token and returns the unencrypted results or fails"""
+    try:
+        decoded = jwt.decode(auth, app.config['SECRET'], algorithms='HS256')
+        output = {}
+        output['username'] = decoded['username']
+        output['email'] = decoded['email']
+
+    except jwt.ExpiredSignatureError:
+        output = 'Access token is missing or invalid'
+    except jwt.DecodeError:
+        output = 'Access token is missing or invalid'
+    return output
+
+def save_to_db(b_type, name, acc, price, amt, stock_inventory, user_inventory, sym):
+    """This function takes buy_sell object and saves it to the db"""
+    if name != '' and acc != '' and float(price) > 0 and int(amt) > 0:
+        if b_type == 'BUY':
+            if stock_inventory - int(amt) > 0:
+                #edit buy_sell values
+                sql = 'INSERT INTO buy_sell(b_type, username, t_account, price, stocktype, quantity) '
+                sql += 'VALUES(\'SELL\', \'admin\', \'Bank Stock Inventory\', \'' + str(price)
+                sql += '\', \'' + sym + '\', \'' + str(amt) + '\'),'
+                sql += '(\'' + b_type + '\', \'' + name + '\', \'' + acc + '\', \''
+                sql += str(price) + '\', \'' + sym + '\', \'' + str(amt) + '\');'
+                query_db(sql)
+
+                #edit account_totals values
+                update_totals(int(amt), float(price), acc, 'BUY', sym)
+                update_totals(int(amt), float(price), 'Bank Stock Inventory', 'SELL', sym)
+
+                return 'Bought from stock inventory'
+
+            #edit buy_sell values
+            required = int(amt) - stock_inventory + 100
+            sql = 'INSERT INTO buy_sell(b_type, username, t_account, price, stocktype, quantity) '
+            sql += 'VALUES(\'BUY\', \'admin\', \'Bank Stock Inventory\', \'' + str(price)
+            sql += '\', \'' + str(required) + '\'),'
+            sql += '(\'SELL\', \'admin\', \'Bank Stock Inventory\', \'' + str(price)
+            sql += '\', \'' + sym + '\', \'' + str(amt) + '\'),'
+            sql += '(\'' + b_type + '\', \'' + name + '\', \'' + acc + '\', \'' + str(price)
+            sql += '\', \'' + sym + '\', \'' + str(amt) + '\');'
+            query_db(sql)
+
+            #edit account_totals values
+            update_totals(int(amt), float(price), acc, 'BUY', sym)
+            update_totals(int(amt), float(price), 'Bank Stock Inventory', 'SELL', sym)
+
+            ret_str = 'Stock inventory overdrawn, inventory bought'
+            ret_str += ' needed amt plus 100 and completed the buy'
+            return ret_str
+
+        if user_inventory - int(amt) >= 0:
+            #edit buy_sell values
+            sql = 'INSERT INTO buy_sell(b_type, username, t_account, price, stocktype, quantity) '
+            sql += 'VALUES(\'BUY\', \'admin\', \'Bank Stock Inventory\', \'' + str(price)
+            sql += '\', \'' + sym + '\', \'' + str(amt) + '\'),'
+            sql += '(\'' + b_type + '\', \'' + name + '\', \'' + acc + '\', \'' + str(price)
+            sql += '\', \'' + sym + '\', \'' + str(amt) + '\');'
+            query_db(sql)
+
+            #edit account_totals values
+            update_totals(int(amt), float(price), acc, 'SELL', sym)
+            update_totals(int(amt), float(price), 'Bank Stock Inventory', 'BUY', sym)
+
+            return 'Sold to stock inventory'
+        return 'User Inventory does not have enough shares to sell the requested amount'
+
+    return 'Invalid order amount or quoted price'
+
+def form_buy_sell_response(b_type, name, acc, price, amt):
+    """Helper function to format the buy_sell JSON response"""
+
+    value = float(price) * int(amt)
+    transaction = json.loads('{}')
+
+    if b_type == 'BUY':
+        output_str = "{\"TransactionType\" : \"BUY\", \"User\" : \"" + name
+        output_str += "\", \"Account\" : \"" + acc + "\", \"Price\" : " + str(price)
+        output_str += ", \"Quantity\" : " + str(amt) + ", \"CostToUser\" : " + str(value)+ "}"
+    elif b_type == 'SELL':
+        output_str = "{\"TransactionType\" : \"SELL\", \"User\" : \"" + name
+        output_str += "\", \"Account\" : \"" + acc + "\", \"Price\" : " + str(price)
+        output_str += ", \"Quantity\" : " + str(amt) + ", \"PaymentToUser\" : " + str(value)+ "}"
+
+    try:
+        transaction = json.loads(output_str)
+    except json.JSONDecodeError:
+        print('Error while forming JSON response for transaction')
+    return transaction
+
+def get_delayed_price(stock):
+    """queries tradier to get the stock price"""
+
+    res = quotes()
+    new_res = res[0]['quotes']['quote']
+    for item in new_res:
+        if item['symbol'] == stock:
+            delayed = round(float(item['last']), 2)
+
+    return delayed
+
+def query_db(sql):
+    """sends a query to the db deciding between a select or insert types"""
+    res = app.config['DB_CONN'].execute(sql)
+
+    if 'SELECT' in sql:
+        res = res.fetchall()
+    return res
+
+######## END BUY/SELL HELPER FUNCTIONS ########
+
+
+
+######## BEGIN DASHBOARD FUNCTIONS ########
+@app.route('/add', methods=["POST"])
+def add_funds():
+    cookie = request.cookies.get('OBS_COOKIE')
+    if cookie == None:
+        return "No User Logged In", 404
+    else:
+        decoded_jwt = authenticate(cookie)
+        if decoded_jwt == 'Access token is missing or invalid':
+            return "No User Logged In", 404
+
+        money_added = request.form.get('money')
+        if float(money_added) > 0:
+            acc = request.form.get('account')
+            sql = 'SELECT dollars FROM account_totals WHERE username = \'' + decoded_jwt['username'] + '\' AND account = \'' + acc + '\';'
+
+            dollars = app.config['DB_CONN'].execute(sql).fetchall()[0][0]
+            new_dollars = round(float(dollars) + float(money_added), 2)
+
+            sql = 'UPDATE account_totals SET dollars = \''
+            sql = sql + str(new_dollars) + '\' WHERE username = \'' + decoded_jwt['username'] + '\' AND account = \'' + acc + '\';'
+
+            res = app.config['DB_CONN'].execute(sql)
+            return 'Funds Sucessfully Added', 200
+
+    return 'Invalid Addition Amount', 500
+
+@app.route('/newacc', methods=["POST"])
+def create_account():
+    cookie = request.cookies.get('OBS_COOKIE')
+    if cookie == None:
+        return "No User Logged In", 404
+    else:
+        decoded_jwt = authenticate(cookie)
+        if decoded_jwt == 'Access token is missing or invalid':
+            return "No User Logged In", 404
+
+        acc_name = request.form.get('account')
+
+        sql = 'SELECT account from account_totals WHERE username = \'' + decoded_jwt['username'] + '\';'
+        current_accounts = app.config['DB_CONN'].execute(sql).fetchall()
+
+        exists = False
+        if len(current_accounts) < 3:
+            for acc in current_accounts:
+                if acc_name == acc[0]:
+                    exists = True
+
+            if not exists:
+                sql = 'INSERT INTO account_totals(account, username) VALUES(\'' + acc_name +  '\', \'' + decoded_jwt['username'] + '\');'
+                app.config['DB_CONN'].execute(sql)
+
+                return 'Account Added Successfully', 200
+
+            return 'Account Already Exists', 500
+
+        return 'User Bank Account Limit Reached', 500
+
+@app.route('/quotes')
+def quotes():
+    conn = http.client.HTTPSConnection('sandbox.tradier.com', 443, timeout=15)
+    bearer_str = 'Bearer ' + 'IymVCsUIpSobaA3RGFqGtWGWzMUQ'
+    headers = {'Accept' : 'application/json', 'Authorization' : bearer_str}
+    quote = json.loads('{}')
+    conn.request('GET', '/v1/markets/quotes?symbols=NTDOY,DIS,ATVI,SGAMY,UBSFY', None, headers)
+    try:
+        res = conn.getresponse()
+        quote = json.loads(res.read().decode('utf-8'))
+    except http.client.HTTPException:
+        return 'Quote request failed', 500
+
+    return quote, 200
+
+@app.route('/totals')
+def total():
+    #try to get the logged in user
+    cookie = request.cookies.get('OBS_COOKIE')
+    if cookie == None:
+        return "No User Logged In", 404
+    else:
+        decoded_jwt = authenticate(cookie)
+        if decoded_jwt == 'Access token is missing or invalid':
+            return "No User Logged In", 404
+
+        #user is logeed in so try and get their dashboard
+        sql = 'SELECT * from account_totals where username = \'' + decoded_jwt['username'] + '\''
+        try:
+            accounts = app.config['DB_CONN'].execute(sql).fetchall()
+            nullAccounts = 3 - len(accounts)
+            #loop through each account to build return array
+            retArray = []
+            for account in accounts:
+                newDict = {
+                    'name': account[0],
+                    'money': account[2],
+                    'ntdoy': account[4],
+                    'sgamy': account[5],
+                    'atvi': account[6],
+                    'dis': account[3],
+                    'ubsfy': account[7]
+                }
+                retArray.append(newDict)
+            #loop through remaining slots to fill in null
+            i = 0
+            while i < nullAccounts:
+                retArray.append(None)
+                i+=1
+            #return as json
+            return jsonify(retArray)
+        except Exception as e:
+            print(e)
+            return 'Database error occurred', 500
+
+@app.route('/buy', methods=['POST'])
+def buy():
+    """take an account and quantity and attempts to purchase that much stock to that account"""
+    auth = request.headers.get('auth')
+    quantity = request.headers.get('quantity')
+    account = request.headers.get('account')
+    stock_symbol = request.headers.get('symbol')
+
+    user_data = authenticate(auth)
+
+    price = get_delayed_price(stock_symbol)
+
+    if isinstance(user_data, dict):
+        stock_col = stock_symbol.lower() + '_stock'
+
+        sql = 'SELECT ' + stock_col + ' FROM account_totals WHERE account = \'Bank Stock Inventory\' AND username = \'admin\''
+        bank_stocks = query_db(sql)[0][0]
+
+        sql = 'SELECT ' + stock_col + ' FROM account_totals WHERE account = \'' + account + '\''
+        user_stocks = query_db(sql)[0][0]
+
+        check = save_to_db('BUY', user_data['username'], account,
+                           price, quantity, bank_stocks,
+                           user_stocks, stock_symbol)
+        if check != 'Invalid order amount or quoted price':
+           buy_res = form_buy_sell_response('BUY', user_data['username'], account, price, quantity)
+           return buy_res, 200
+
+        return check, 500
+
+    return user_data, 401
+
+@app.route('/sell', methods=['POST'])
+def sell():
+    """take an account and quantity and attempts to sell that much stock from that account"""
+    auth = request.headers.get('auth')
+    quantity = request.headers.get('quantity')
+    account = request.headers.get('account')
+    stock_symbol = request.headers.get('symbol')
+
+    user_data = authenticate(auth)
+
+    price = get_delayed_price(stock_symbol)
+
+    if isinstance(user_data, dict):
+        stock_col = stock_symbol.lower() + '_stock'
+
+        sql = 'SELECT ' + stock_col + ' FROM account_totals WHERE account = \'' + account + '\''
+        user_stocks = query_db(sql)[0][0]
+
+        check = save_to_db('SELL', user_data['username'], account, price,
+                           quantity, 5000, user_stocks, stock_symbol)
+        if (check != 'User Inventory does not have enough shares to sell the requested amount'
+        and check != 'Invalid order amount or quoted price'):
+
+           sell_res = form_buy_sell_response('SELL', user_data['username'], account, price, quantity)
+           return sell_res, 200
+
+        return check, 500
+
+    return user_data, 401
+
+######## END DASHBOARD FUNCTIONS ########
+
+
+
+######## BEGIN USER ROUTES ########
 @app.route('/')
 def home():
     return render_template("obs_navigation.html")
@@ -88,140 +408,15 @@ def login():
         else:
             return "Invalid User Credentials", 400
 
-def authenticate(auth):
-    """This function takes a token and returns the unencrypted results or fails"""
-    try:
-        decoded = jwt.decode(auth, app.config['SECRET'], algorithms='HS256')
-        output = {}
-        output['username'] = decoded['username']
-        output['email'] = decoded['email']
-
-    except jwt.ExpiredSignatureError:
-        output = 'Access token is missing or invalid'
-    except jwt.DecodeError:
-        output = 'Access token is missing or invalid'
-    return output
-
-@app.route('/add', methods=["POST"])
-def add_funds():
-    cookie = request.cookies.get('OBS_COOKIE')
-    if cookie == None:
-        return "No User Logged In", 404
-    else:
-        decoded_jwt = authenticate(cookie)
-        if decoded_jwt == 'Access token is missing or invalid':
-            return "No User Logged In", 404
-
-        money_added = request.form.get('money')
-        if float(money_added) > 0:
-            acc = request.form.get('account')
-            sql = 'SELECT dollars FROM account_totals WHERE username = \'' + decoded_jwt['username'] + '\' AND account = \'' + acc + '\';'
-
-            dollars = app.config['DB_CONN'].execute(sql).fetchall()[0][0]
-            new_dollars = round(float(dollars) + float(money_added), 2)
-
-            sql = 'UPDATE account_totals SET dollars = \''
-            sql = sql + str(new_dollars) + '\' WHERE username = \'' + decoded_jwt['username'] + '\' AND account = \'' + acc + '\';'
-
-            res = app.config['DB_CONN'].execute(sql)
-            return 'Funds Sucessfully Added', 200
-
-    return 'Invalid Addition Amount', 500
-
-@app.route('/newacc', methods=["POST"])
-def create_account():
-    cookie = request.cookies.get('OBS_COOKIE')
-    if cookie == None:
-        return "No User Logged In", 404
-    else:
-        decoded_jwt = authenticate(cookie)
-        if decoded_jwt == 'Access token is missing or invalid':
-            return "No User Logged In", 404
-
-        acc_name = request.form.get('account')
-
-        sql = 'SELECT account from account_totals WHERE username = \'' + decoded_jwt['username'] + '\';'
-        current_accounts = app.config['DB_CONN'].execute(sql).fetchall()
-
-        exists = False
-        if len(current_accounts) < 3:
-            for acc in current_accounts:
-                if acc_name == acc[0]:
-                    exists = True
-
-            if not exists:
-                sql = 'INSERT INTO account_totals(account, username) VALUES(\'' + acc_name +  '\', \'' + decoded_jwt['username'] + '\');'
-                app.config['DB_CONN'].execute(sql)
-
-                return 'Account Added Successfully', 200
-
-            return 'Account Already Exists', 500
-
-        return 'User Bank Account Limit Reached', 500
-
-
 @app.route('/dashboard')
 def dashboard():
     return render_template("dashboard.html")
 
-@app.route('/quotes')
-def quotes():
-    conn = http.client.HTTPSConnection('sandbox.tradier.com', 443, timeout=15)
-    bearer_str = 'Bearer ' + 'IymVCsUIpSobaA3RGFqGtWGWzMUQ'
-    headers = {'Accept' : 'application/json', 'Authorization' : bearer_str}
-    quote = json.loads('{}')
-    conn.request('GET', '/v1/markets/quotes?symbols=NTDOY,DIS,ATVI,SGAMY,UBSFY', None, headers)
-    try:
-        res = conn.getresponse()
-        quote = json.loads(res.read().decode('utf-8'))
-    except http.client.HTTPException:
-        return 'Quote request failed', 500
-
-    return quote, 200
-
-@app.route('/totals')
-def total():
-    #try to get the logged in user
-    cookie = request.cookies.get('OBS_COOKIE')
-    if cookie == None:
-        return "No User Logged In", 404
-    else:
-        decoded_jwt = authenticate(cookie)
-        if decoded_jwt == 'Access token is missing or invalid':
-            return "No User Logged In", 404
-
-        #user is logeed in so try and get their dashboard
-        sql = 'SELECT * from account_totals where username = \'' + decoded_jwt['username'] + '\''
-        try:
-            accounts = app.config['DB_CONN'].execute(sql).fetchall()
-            nullAccounts = 3 - len(accounts)
-            #loop through each account to build return array
-            retArray = []
-            for account in accounts:
-                newDict = {
-                    'name': account[0],
-                    'money': account[2],
-                    'ntdoy': account[4],
-                    'sgamy': account[5],
-                    'atvi': account[6],
-                    'dis': account[3],
-                    'ubsfy': account[7]
-                }
-                retArray.append(newDict)
-            #loop through remaining slots to fill in null
-            i = 0
-            while i < nullAccounts:
-                retArray.append(None)
-                i+=1
-            #return as json
-            return jsonify(retArray)
-        except Exception as e:
-            print(e)
-            return 'Database error occurred', 500
-
 @app.route('/welcome')
 def welcome():
     return render_template("obs_home.html")
+
+######## END USER ROUTES ########
 
 if __name__ == "__main__" :
 
